9984c020c4e844858b5732a5a04869384d101412
Polyxen1a <maslowa@inbox.ru>
2023-03-02 16:37:42 +0300

    Initial commit

---
+++ src/main/java/pro/sky/course_work_on_the_3rd_course_final_1/CourseWorkOnThe3rdCourseFinal1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work_on_the_3rd_course_final_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOnThe3rdCourseFinal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWorkOnThe3rdCourseFinal1Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/course_work_on_the_3rd_course_final_1/CourseWorkOnThe3rdCourseFinal1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course_work_on_the_3rd_course_final_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOnThe3rdCourseFinal1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
